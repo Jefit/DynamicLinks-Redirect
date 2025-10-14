2831f7aa3f9e06f5907a2b4c478c5b8620f54c06
sean <48690148+sean-jefit@users.noreply.github.com>
2025-10-14 10:01:43 -0700

    set default icon url to static file

--- config/config.go
+++ config/config.go
@@ -22,7 +22,7 @@ func New() *Config {
 		Port:                      getEnv("PORT", "4040"),
 		PreviewUrlStyle:           getEnv("PREVIEW_URL_STYLE", "hyphenated"), // hyphenated or subdomain
 		ExchangeShortLinkEndpoint: getEnv("EXCHANGE_SHORT_LINK_ENDPOINT", "http://localhost:9010/v1/exchangeShortLink"),
-		AppIconImageURL:           getEnv("APP_ICON_IMAGE_URL", "https://example.com/icon.png"),
+		AppIconImageURL:           getEnv("APP_ICON_IMAGE_URL", "/static/appIcon.svg"),
 		AppName:                   getEnv("APP_NAME", "My app name"),
 		SSLEnabled:                getEnv("SSL_ENABLED", "false"),
 		SSLCertPath:               getEnv("SSL_CERT_PATH", "./tls.crt"),
